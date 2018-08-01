5e60f960fdc81de635aef21cefa612ba67be9208
Brandon Shewmake <shewstr@gmail.com>
2018-08-01 04:24:21 -0500

    [minor] title bug-fix

--- src/Stories.js
+++ src/Stories.js
@@ -25,7 +25,7 @@ class Stories extends Component {
       return category.key === (params.category || "anything");
     })[0];
 
-    document.title = "Find Royalty-Free Music for " + params.category.name + " - Etherchime";
+    document.title = "Find Royalty-Free Music for " + category.name + " - Etherchime";
 
     this.setState({
       category: category
